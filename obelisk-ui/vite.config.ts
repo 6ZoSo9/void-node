c9e12c4d53bd9cafd617905db2f43aacfae4282f
ZoSo <derrekdaly@gmail.com>
2025-12-14 00:25:00 -0600

    obelisk-ui: vite proxy for /__void_helper -> devnet helper

--- obelisk-ui/vite.config.ts
+++ obelisk-ui/vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
     port: 5173,
     host: "127.0.0.1",
     proxy: {
+      '/__void_helper': {
+        target: 'http://127.0.0.1:4312',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/__void_helper/, ''),
+      },
       // Anything under /workcredits/* goes to the helper on :4312
       "/workcredits": {
         target: "http://127.0.0.1:4312",
