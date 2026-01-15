b2a8f888ea1a19ee6a9c29eb0b71c2882a409c34
ZoSo <derrekdaly@gmail.com>
2026-01-14 21:24:41 -0600

    ops: prepush-strict prints rev breadcrumb

--- ops/bin/void-prepush-strict.sh
+++ ops/bin/void-prepush-strict.sh
@@ -21,3 +21,5 @@ export PROM
 export VOID_ENFORCE_PILLARS_ADDONS="${VOID_ENFORCE_PILLARS_ADDONS:-1}"
 
 exec bash ops/bin/void-proposer-v3b-pillars-check.sh
+
+echo "[ok] prepush-strict $(date -Is) rev=$(git rev-parse --short HEAD)"
